6cd7ad09339fc0d879c2150bf7c2ab0f2ecda90a
luc-dev10 <luc.dev1008@gmail.com>
2022-02-26 12:49:18 -0500

    add linked list Node

--- src/com/algo/list/Node.java
+++ src/com/algo/list/Node.java
@@ -4,5 +4,24 @@ public class Node<E> {
 
     private E value;
     private Node<E> next;
-    
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
