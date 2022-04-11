98a6d27312a396f12a7c7ee34ec6ff42187aef60
luc-dev10 <luc.dev1008@gmail.com>
2022-04-10 22:15:48 -0400

    add binary search

---
+++ src/com/algo/search/BinarySearchApp.java
@@ -0,0 +1,36 @@
+package com.algo.search;
+
+public class BinarySearchApp {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[400];
+        for (int i = 0; i < intArray.length; i++)
+            intArray[i] = i * 100;
+
+        // check if number exists
+        System.out.println(getIndexOfItems(intArray, 900));
+
+    }
+
+    public static int getIndexOfItems(int[] items, int number) {
+
+        int mid = items.length / 2;
+        int end = items.length - 1;
+        int start = 0;
+
+        while (start != end) {
+
+            if (number == items[mid])
+                return mid;
+
+            if (number > items[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+
+            mid = (end + start) / 2;
+        }
+
+        return -1;
+    }
+}
