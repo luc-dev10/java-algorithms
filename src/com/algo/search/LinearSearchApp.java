d26cabdb23746f3605479173687f11993d41d438
luc-dev10 <luc.dev1008@gmail.com>
2022-04-10 21:57:23 -0400

    add linear search

---
+++ src/com/algo/search/LinearSearchApp.java
@@ -0,0 +1,24 @@
+package com.algo.search;
+
+public class LinearSearchApp {
+    public static void main(String[] args) {
+        int[] intArray = new int[400];
+        for (int i = 0; i < intArray.length; i++)
+            intArray[i] = i * 100;
+
+        // check if number exists
+        System.out.println(getItemOfIndex(intArray, 5900));
+
+    }
+
+    // linear search
+    public static int getItemOfIndex(int[] intArray, int number) {
+
+        for (int i = 0; i < intArray.length; i++)
+            if (intArray[i] == number)
+                return i;
+
+        return -1;
+    }
+
+}
