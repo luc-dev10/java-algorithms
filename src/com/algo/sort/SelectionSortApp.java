d3d2a414ebc7e7c1917bdd79f3da6b84b599af9a
luc-dev10 <luc.dev1008@gmail.com>
2022-04-14 22:15:12 -0400

    Add Selection Sort

---
+++ src/com/algo/sort/SelectionSortApp.java
@@ -0,0 +1,30 @@
+package com.algo.sort;
+
+public class SelectionSortApp {
+    public static void main(String[] args) {
+
+        // create list
+        int[] numbers = new int[]{3, 6, 10, 7, 1, 4, 10, 12};
+        selectionSort(numbers);
+
+        for (int number : numbers)
+            System.out.println(number);
+
+    }
+
+    public static void selectionSort(int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int indexOfLowest = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[indexOfLowest])
+                    indexOfLowest = j;
+            }
+            int temp = numbers[i];
+            numbers[i] = numbers[indexOfLowest];
+            numbers[indexOfLowest] = temp;
+        }
+
+    }
+}
