61ca3773d5d6170babf9ed301f489022d2b453c4
luc-dev10 <luc.dev1008@gmail.com>
2022-04-23 21:24:15 -0400

    add factorial solution

---
+++ src/com/algo/recursion/Factorial.java
@@ -0,0 +1,16 @@
+package com.algo.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int factorialNumber = factorial(4);
+        System.out.println(factorialNumber);
+    }
+
+    private static int factorial(int number) {
+        if (number == 1)
+            return 1;
+
+        return number * factorial(--number);
+    }
+
+}
