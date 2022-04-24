60d756b8f188b6a7b643ccf27ac75accc436fafe
luc-dev10 <luc.dev1008@gmail.com>
2022-04-23 20:58:13 -0400

    iteration with recursion

---
+++ src/com/algo/recursion/RecursiveIteration.java
@@ -0,0 +1,18 @@
+package com.algo.recursion;
+
+public class RecursiveIteration {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 3, 5, 7, 4, 2};
+        iterate(numbers, 0);
+    }
+
+    public static void iterate(int[] numbers, int index) {
+
+        // base case
+        if (numbers.length <= index)
+            return;
+
+        System.out.println(numbers[index]);
+        iterate(numbers, ++index);
+    }
+}
