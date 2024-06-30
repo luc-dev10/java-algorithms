b69663ee2066dccfa590e2201c6116e24d775dd3
Lucio Zhao <luc.dev1008@gmail.com>
2024-06-29 22:05:36 -0400

    adds length test for queue

--- src/test/java/com/lucio/algo/structure/queue/QueueTest.java
+++ src/test/java/com/lucio/algo/structure/queue/QueueTest.java
@@ -1,6 +1,9 @@
 package com.lucio.algo.structure.queue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QueueTest {
     Queue<Integer> queue;
@@ -15,5 +18,15 @@ public class QueueTest {
         for (int i = 0; i <= 10; i++) {
             queue.enqueue(i);
         }
+    }
+
+    @Test()
+    @DisplayName("Test length of integer queue")
+    public void sizeTest() {
+        int expectedSizeOfFilledQueue = 11;
+        int expectedSizeOfEmptyQueue = 0;
+
+        Assertions.assertEquals(this.queue.getSize(), expectedSizeOfFilledQueue);
+        Assertions.assertEquals(this.emptyQueue.getSize(), expectedSizeOfEmptyQueue);
     }
 }
