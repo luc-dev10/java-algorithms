0774848add7fba1b7509f7c336f463a08a54bb13
Lucio Zhao <luc.dev1008@gmail.com>
2024-06-29 21:41:13 -0400

    refactor stack class

--- src/main/java/com/lucio/algo/structure/stack/Stack.java
+++ src/main/java/com/lucio/algo/structure/stack/Stack.java
@@ -3,9 +3,8 @@ package com.lucio.algo.structure.stack;
 import com.lucio.algo.structure.list.DoublyList;
 import com.lucio.algo.structure.node.DoublyNode;
 
-// LIFO = Last Element Added to Stack is the First Element Removed
+// LIFO
 public class Stack<E> {
-
     private final DoublyList<E> doublyList = new DoublyList<>();
 
     // pop - O(1)
@@ -33,15 +32,4 @@ public class Stack<E> {
     public void set(int index, E value) {
         doublyList.set(index, value);
     }
-    //
-    // @Override
-    // public String toString() {
-    //
-    //     StringBuilder stringBuilder = new StringBuilder();
-    //     for (E value : doublyList)
-    //         stringBuilder.append(value)
-    //                 .append("\n");
-    //
-    //     return stringBuilder.toString();
-    // }
 }
