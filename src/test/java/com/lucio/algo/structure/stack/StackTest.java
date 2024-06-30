0d352cbc32fa5a3850244ffe4ac4ee5b5cef870e
Lucio Zhao <luc.dev1008@gmail.com>
2024-06-29 21:45:18 -0400

    adds size test for stack

--- src/test/java/com/lucio/algo/structure/stack/StackTest.java
+++ src/test/java/com/lucio/algo/structure/stack/StackTest.java
@@ -1,6 +1,9 @@
 package com.lucio.algo.structure.stack;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StackTest {
     Stack<Integer> stack;
@@ -15,6 +18,16 @@ public class StackTest {
         for (int i = 0; i <= 10; i++) {
             stack.push(i);
         }
+    }
+
+    @Test()
+    @DisplayName("Test length of integer stack")
+    public void sizeOfLinkedListExpectingElevenToReturnTest() {
+        int expectedSizeForFilledStack = 10;
+        int expectedSizeForEmptyStack = 0;
+
+        Assertions.assertEquals(this.stack.getSize(), expectedSizeForFilledStack);
+        Assertions.assertNotEquals(this.emptyStack.getSize(), expectedSizeForEmptyStack);
     }
 }
 
