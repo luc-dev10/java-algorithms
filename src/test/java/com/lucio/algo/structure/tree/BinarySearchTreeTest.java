e712e20a71f25626c249b0cf736127f5232a9c65
Lucio Zhao <luc.dev1008@gmail.com>
2024-07-07 12:48:59 -0400

    Adds in-order test for BST

--- src/test/java/com/lucio/algo/structure/tree/BinarySearchTreeTest.java
+++ src/test/java/com/lucio/algo/structure/tree/BinarySearchTreeTest.java
@@ -124,4 +124,31 @@ public class BinarySearchTreeTest {
             return true;
         });
     }
+
+    @Test()
+    @DisplayName("Test In Order Depth first")
+    public void inOrderDepthFirstTest() {
+        SinglyList<Integer> expectedSinglyList = new SinglyList<>();
+        expectedSinglyList.push(12);
+        expectedSinglyList.push(15);
+        expectedSinglyList.push(17);
+        expectedSinglyList.push(20);
+        expectedSinglyList.push(35);
+        expectedSinglyList.push(40);
+        expectedSinglyList.push(80);
+
+        SinglyList<Integer> singlyList = this.binarySearchTree.getInOrderDepthFirst();
+
+        Assertions.assertTrue(() -> {
+            for (int i = 0; i < expectedSinglyList.getSize(); i++) {
+                int currentValue = singlyList.get(i)
+                        .getValue();
+                int expectedValue = expectedSinglyList.get(i)
+                        .getValue();
+
+                if (currentValue != expectedValue) return false;
+            }
+            return true;
+        });
+    }
 }
