6dbe3adb12e537b5da1014a10887fff074f35b45
Lucio Zhao <luc.dev1008@gmail.com>
2024-07-06 20:45:47 -0400

    Adds insertion for BST

--- src/main/java/com/lucio/algo/structure/tree/BinarySearchTree.java
+++ src/main/java/com/lucio/algo/structure/tree/BinarySearchTree.java
@@ -2,10 +2,29 @@ package com.lucio.algo.structure.tree;
 
 import com.lucio.algo.structure.node.BinarySearchTreeNode;
 
-public class BinarySearchTree<T> {
-    BinarySearchTreeNode<T> root = null;
+public class BinarySearchTree<T extends Comparable<T>> {
+    BinarySearchTreeNode<T> root;
 
-    public BinarySearchTree(T value) {
-        this.root = new BinarySearchTreeNode<>(value);
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    // Insertion
+    public void insert(T value) {
+        this.insertRecursively(this.root, value);
+    }
+
+    private void insertRecursively(BinarySearchTreeNode<T> currentNode, T value) {
+        if (currentNode == null) {
+            this.root = new BinarySearchTreeNode<>(value);
+            return;
+        }
+
+        // check where to add
+        int comparison = currentNode.getValue()
+                .compareTo(value);
+
+        if (comparison > 0) insertRecursively(currentNode.getLeft(), value);
+        else if (comparison < 0) insertRecursively(currentNode.getRight(), value);
     }
 }
