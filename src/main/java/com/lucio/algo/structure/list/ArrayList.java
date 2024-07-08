702490d91c12e2645263a865645421309c687cb7
Lucio Zhao <luc.dev1008@gmail.com>
2024-07-08 18:59:54 -0400

    Implements Iterable in Array List

--- src/main/java/com/lucio/algo/structure/list/ArrayList.java
+++ src/main/java/com/lucio/algo/structure/list/ArrayList.java
@@ -1,12 +1,31 @@
 package com.lucio.algo.structure.list;
 
+import java.util.Iterator;
+
 @SuppressWarnings("unchecked cast")
-public class ArrayList<T extends Comparable<T>> {
+public class ArrayList<T extends Comparable<T>> implements Iterable<T> {
     private int size;
     private Object[] values;
 
     public ArrayList() {
         int INITIAL_CAPACITY = 5;
         values = new Object[INITIAL_CAPACITY];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) values[index++];
+            }
+        };
     }
 }
