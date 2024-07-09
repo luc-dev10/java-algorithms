d512d11b490c81c4e2af11897b035d7ad3d5a0b3
Lucio Zhao <luc.dev1008@gmail.com>
2024-07-09 19:54:07 -0400

    adds length test in Array List

--- src/test/java/com/lucio/algo/structure/list/ArrayListTest.java
+++ src/test/java/com/lucio/algo/structure/list/ArrayListTest.java
@@ -1,6 +1,9 @@
 package com.lucio.algo.structure.list;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class ArrayListTest {
     ArrayList<Integer> arrayList;
@@ -16,4 +19,12 @@ public class ArrayListTest {
             arrayList.add(i);
         }
     }
+
+    @Test()
+    @DisplayName("Test length")
+    public void lengthTest() {
+        int expectedSize = 11;
+        Assertions.assertEquals(this.arrayList.getSize(), expectedSize);
+    }
+
 }
