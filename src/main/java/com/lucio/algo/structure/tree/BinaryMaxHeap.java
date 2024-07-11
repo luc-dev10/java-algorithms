5bf94ff53595abd3e4f0b3f819ddee91a6fda603
Lucio Zhao <luc.dev1008@gmail.com>
2024-07-10 21:37:44 -0400

    sets up binary max heap

---
+++ src/main/java/com/lucio/algo/structure/tree/BinaryMaxHeap.java
@@ -0,0 +1,18 @@
+package com.lucio.algo.structure.tree;
+
+import com.lucio.algo.structure.list.ArrayList;
+
+import java.util.Iterator;
+
+public class BinaryMaxHeap<T extends Comparable<T>> implements Iterable<T> {
+    private final ArrayList<T> values;
+
+    public BinaryMaxHeap() {
+        this.values = new ArrayList<>();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return this.values.iterator();
+    }
+}
